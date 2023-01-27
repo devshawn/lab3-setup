418d51f7adbd15c976663bd64d06fe486e1a16bf
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2023-01-26 23:43:11 -0600

    Remove empty UserCardSpec
    Removed the UserCardComponent.spec file since it was empty, and causing checks to fail.

--- client/src/app/users/user-card.component.spec.ts
+++
@@ -1,6 +0,0 @@
-describe('UserCardComponent is trivial, so we do not actually test it', () => {
-  /*
-   * The `UserCardComponent` has no actually logic in
-   * it, so there's really nothing to test here.
-   */
-});
