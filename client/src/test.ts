18614f920edd2dd4baede26008846d0d7565beee
K.K. Lamberty <lamberty@morris.umn.edu>
2023-02-06 23:06:57 -0600

    Set up karma tests
    Take this file from the iteration template

--- client/src/test.ts
+++ client/src/test.ts
@@ -1,18 +1,11 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/testing';
+import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
-
-declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
-    <T>(id: string): T;
-    keys(): string[];
-  };
-};
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
@@ -22,7 +15,3 @@ getTestBed().initTestEnvironment(
     teardown: { destroyAfterEach: false }
   }
 );
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
